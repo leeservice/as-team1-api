44e1f4ee07edf8d298a4a61023311d41ccce01f3
leeservice <lee.service@kainos.com>
2023-07-18 15:17:39 +0100

    testing jhusky with mvn spotless

--- src/main/java/org/kainos/ea/api/main.java
+++ src/main/java/org/kainos/ea/api/main.java
@@ -3,7 +3,6 @@ package org.kainos.ea.api;
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("hello!");;;;;;
-        ;
+                        System.out.println("hello!");
     }
 }
