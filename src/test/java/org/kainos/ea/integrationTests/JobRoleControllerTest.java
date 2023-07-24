9eb5071a38f929250b612a76cce75f6cecca1463
cderry1 <conor.derry@kainos.com>
2023-07-24 14:58:17 +0100

    Added Delete Swagger End Points with the unit and intgration tests

--- src/test/java/org/kainos/ea/integrationTests/JobRoleControllerTest.java
+++ src/test/java/org/kainos/ea/integrationTests/JobRoleControllerTest.java
@@ -2,7 +2,9 @@ package org.kainos.ea.integrationTests;
 
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.model.JobRole;
 import org.kainos.ea.trueApplication;
 import org.kainos.ea.trueConfiguration;
@@ -14,13 +16,13 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@ExtendWith(DropwizardExtensionsSupport.class)
 public class JobRoleControllerTest {
     static final DropwizardAppExtension<trueConfiguration> APP = new DropwizardAppExtension<>(trueApplication.class, null, new ResourceConfigurationSourceProvider());
 
     @Test
     void DeleteJobRole_shouldReturnDeletionJobRoles() {
-        Response response = APP.client().target("http://localhost:8080job-roles/1").request(MediaType.APPLICATION_JSON).delete();
+        Response response = APP.client().target("http://localhost:8080/api/job-roles/1").request().delete();
 
         assertEquals(200, response.getStatus());
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
