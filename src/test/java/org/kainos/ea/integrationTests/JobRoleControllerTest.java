256ee2b65dea88a1217a7ff4a8e193c67deba3ce
ryanbeatt <137309439+ryanbeatt@users.noreply.github.com>
2023-07-24 11:18:12 +0100

    Made requested changes to us001. Refactored tests.

--- src/test/java/org/kainos/ea/integrationTests/JobRoleControllerTest.java
+++ src/test/java/org/kainos/ea/integrationTests/JobRoleControllerTest.java
@@ -1,9 +1,6 @@
 package org.kainos.ea.integrationTests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
@@ -11,8 +8,6 @@ import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 
 import java.util.List;
 import javax.ws.rs.core.Response;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.model.JobRole;
@@ -26,13 +21,12 @@ public class JobRoleControllerTest {
 
     @Test
     void getJobRoles_shouldReturnListOfJobRoles() {
-        List<JobRole> response = APP.client().target("http://localhost:8080/api/job-roles").request().get(List.class);
-        Assertions.assertTrue(response.size() > 0);
-    }
-
-    @Test
-    void getJobRoles_shouldReturn200_whenJobRoleListReturned() {
         Response response = APP.client().target("http://localhost:8080/api/job-roles").request().get();
-        Assertions.assertEquals(200, response.getStatus());
+
+        //Checks for 200 response code
+        assertEquals(200, response.getStatus());
+        List<JobRole> jobRoles = response.readEntity(List.class);
+        //Checks if list returned has a size greater than 0
+        assertTrue(jobRoles.size() > 0);
     }
 }
