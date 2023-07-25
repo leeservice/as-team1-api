05fe99f68604f1e5973abd3be25d6a34e9bc62fa
cderry1 <conor.derry@kainos.com>
2023-07-25 15:57:27 +0100

    Merged to main

--- src/main/java/org/kainos/ea/trueConfiguration.java
+++ src/main/java/org/kainos/ea/trueConfiguration.java
@@ -7,22 +7,9 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 public class trueConfiguration extends Configuration {
-<<<<<<< HEAD
   @Valid
   @NotNull
   private final SwaggerBundleConfiguration swagger = new SwaggerBundleConfiguration();
-
-  @JsonProperty("swagger")
-  public SwaggerBundleConfiguration getSwagger(){
-    swagger.setResourcePackage("org.kainos.ea.controller");
-    String[] schemes = {"http", "https"};
-    swagger.setSchemes(schemes);
-    return swagger;
-  }
-=======
-    @Valid @NotNull
-    private final SwaggerBundleConfiguration swagger = new SwaggerBundleConfiguration();
->>>>>>> c42ba9ac65a91f8ba3eb550ae719fbe249f2f073
 
     @JsonProperty("swagger")
     public SwaggerBundleConfiguration getSwagger() {
