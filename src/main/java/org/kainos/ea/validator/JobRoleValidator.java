937fe5b011ecd3db17440502febaffb11d20165e
ryanbeatt <137309439+ryanbeatt@users.noreply.github.com>
2023-07-25 16:02:35 +0100

    Renamed JobRolePostRequest to JobRoleRequest

--- src/main/java/org/kainos/ea/validator/JobRoleValidator.java
+++ src/main/java/org/kainos/ea/validator/JobRoleValidator.java
@@ -4,6 +4,9 @@ import org.kainos.ea.model.JobRoleRequest;
 
 public class JobRoleValidator {
     public boolean isValidJobRole(JobRoleRequest jobRoleRequest) {
+        if (jobRoleRequest.getName().length() > 50) {
+            return false;
+        }
         return true;
     }
 }
