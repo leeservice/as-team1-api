a2dd0239e7dece21b8b98bf58749de0572387e03
cderry1 <conor.derry@kainos.com>
2023-07-25 14:33:34 +0100

    Removed Banding changes

--- src/test/java/org/kainos/ea/controllerTests/JobRoleControllerTest.java
+++ src/test/java/org/kainos/ea/controllerTests/JobRoleControllerTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.controller.JobRoleController;
 import org.kainos.ea.dao.JobRoleDao;
+import org.kainos.ea.exceptions.DatabaseConnectionException;
 import org.kainos.ea.exceptions.FailedToGetJobRoleException;
+import org.kainos.ea.exceptions.JobRoleDoesNotExistException;
 import org.kainos.ea.service.JobRoleService;
 import org.kainos.ea.utility.DatabaseConnector;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -22,6 +26,11 @@ public class JobRoleControllerTest {
     void getAllJobRoles_shouldReturn500_whenJobRoleServiceThrowsFailedToGetJobRoleException() throws FailedToGetJobRoleException {
         when(jobRoleService.getAllJobRoles()).thenThrow(FailedToGetJobRoleException.class);
         assertEquals(500, jobRoleController.getAllJobRoles().getStatus());
+    }
+    @Test
+    void DeleteJobRoles_shouldReturn500_whenJobRoleServiceThrowsFailedToGetJobRoleException() throws FailedToGetJobRoleException, JobRoleDoesNotExistException, DatabaseConnectionException, SQLException {
+        when(jobRoleService.deleteJob(1)).thenThrow(FailedToGetJobRoleException.class);
+        assertEquals(500, jobRoleController.deleteJobRole(1).getStatus());
     }
 
 }
