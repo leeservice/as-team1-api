71dc6e17639b460899185eb4e37e45fe9f1a3fa8
cderry1 <conor.derry@kainos.com>
2023-07-25 16:22:58 +0100

    Problems with merging

--- src/test/java/org/kainos/ea/integrationTests/JobRoleControllerTest.java
+++ src/test/java/org/kainos/ea/integrationTests/JobRoleControllerTest.java
@@ -1,22 +1,30 @@
 package org.kainos.ea.integrationTests;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
-
-import java.util.List;
-import javax.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.dao.JobRoleDao;
+import org.kainos.ea.exceptions.DatabaseConnectionException;
 import org.kainos.ea.model.JobRole;
+import org.kainos.ea.model.JobRoleRequest;
 import org.kainos.ea.trueApplication;
 import org.kainos.ea.trueConfiguration;
+import org.kainos.ea.utility.DatabaseConnector;
 
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class JobRoleControllerTest {
-
     static final DropwizardAppExtension<trueConfiguration> APP = new DropwizardAppExtension<>(trueApplication.class, null, new ResourceConfigurationSourceProvider());
 
     @Test
@@ -24,11 +32,14 @@ public class JobRoleControllerTest {
         Response response = APP.client().target("http://localhost:8080/api/job-roles").request().get();
 
         //Checks for 200 response code
-        assertEquals(200, response.getStatus());
-        List<JobRole> jobRoles = response.readEntity(List.class);
-        //Checks if list returned has a size greater than 0
+        List<JobRoleRequest> jobRoles = response.readEntity(new GenericType<List<JobRoleRequest>>() {
+        });
         assertTrue(jobRoles.size() > 0);
-
-
+        JobRoleRequest jobRoleResponse = jobRoles.get(0);
+        String expected = "Engineering";
+        String actual = jobRoleResponse.getCapability();
+        assertEquals(expected, actual);
     }
+
 }
+
