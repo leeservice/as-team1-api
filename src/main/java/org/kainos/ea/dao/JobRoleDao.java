1917936cc5afc9c442af4bff26255596f31155ca
ryanbeatt <137309439+ryanbeatt@users.noreply.github.com>
2023-07-26 09:21:03 +0100

    US003 - view capability for each job role (#22)
    * US003 - view capability for each job role
    
    * Made suggested changes to US003 - view capability a role belongs to
    
    * Removed assertions in service tests

--- src/main/java/org/kainos/ea/dao/JobRoleDao.java
+++ src/main/java/org/kainos/ea/dao/JobRoleDao.java
@@ -6,21 +6,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.kainos.ea.model.JobRole;
+import org.kainos.ea.model.JobRoleResponse;
 
 public class JobRoleDao {
-    public List<JobRole> getAllJobRoles(Connection c) throws SQLException {
+    public List<JobRoleResponse> getAllJobRoles(Connection c) throws SQLException {
         Statement st = c.createStatement();
 
-        ResultSet rs = st.executeQuery("SELECT Job_Roles.id AS 'ID', Job_Roles.`name` AS 'Name'" + " FROM Job_Roles");
+        ResultSet rs = st.executeQuery("SELECT Job_Roles.id AS 'ID', Job_Roles.`name` AS 'Name', Capability.id AS 'Capability ID', Capability.`name` AS 'Capability Name'" +
+                " FROM Job_Roles" +
+                " INNER JOIN Capability ON Job_Roles.capability_id = Capability.id;");
 
-        List<JobRole> jobRoleList = new ArrayList<>();
+        List<JobRoleResponse> jobRoleResponseList = new ArrayList<>();
 
         while (rs.next()) {
-            JobRole jobRole = new JobRole(rs.getInt("ID"), rs.getString("Name"));
-            jobRoleList.add(jobRole);
+            JobRoleResponse jobRoleResponse = new JobRoleResponse(rs.getInt("ID"), rs.getString("Name"), rs.getString("Capability Name"));
+            jobRoleResponseList.add(jobRoleResponse);
         }
-        return jobRoleList;
+        return jobRoleResponseList;
     }
 }
