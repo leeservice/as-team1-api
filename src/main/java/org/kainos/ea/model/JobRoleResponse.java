aaa1021824fbf6ad81d9d7a72a9a04990613c93b
leeservice <137312132+leeservice@users.noreply.github.com>
2023-07-26 16:36:23 +0100

    Us002 view job role spec (#23)
    * Adding url and spec to job role
    
    * Adding url and spec to job role
    
    * Added SQL properties in correct order - all working
    
    * adding json creator to jobrole response for swagger documentation

--- src/main/java/org/kainos/ea/model/JobRoleResponse.java
+++ src/main/java/org/kainos/ea/model/JobRoleResponse.java
@@ -1,19 +1,27 @@
 package org.kainos.ea.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class JobRoleResponse {
     private int id;
     private String name;
     private String capability;
+    private String urlLink;
+    private String specificationDescription;
 
+    @JsonCreator
     public JobRoleResponse(
             @JsonProperty("id") int id,
             @JsonProperty("name") String name,
-            @JsonProperty("capability") String capability) {
+            @JsonProperty("capability") String capability,
+            @JsonProperty("urlLink") String urlLink,
+            @JsonProperty("specificationDescription") String specificationDescription) {
         setId(id);
         setName(name);
         setCapability(capability);
+        setSpecificationDescription(specificationDescription);
+        setUrlLink(urlLink);
     }
 
     public int getId() {
@@ -40,16 +48,30 @@ public class JobRoleResponse {
         this.capability = capability;
     }
 
-    @Override
-    public String toString() {
-        return "JobRoleResponse{"
-                + "id="
-                + id
-                + ", name='"
-                + name
-                + '\''
-                + ", capability="
-                + capability
-                + '}';
+    public String getUrlLink() {
+        return urlLink;
     }
+
+    public void setUrlLink(String urlLink) {
+        this.urlLink = urlLink;
+    }
+
+    public String getSpecificationDescription() {
+        return specificationDescription;
+    }
+
+    public void setSpecificationDescription(String specificationDescription) {
+        this.specificationDescription = specificationDescription;
+    }
+
+  @Override
+  public String toString() {
+    return "JobRoleResponse{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", capability='" + capability + '\'' +
+        ", urlLink='" + urlLink + '\'' +
+        ", specificationDescription='" + specificationDescription + '\'' +
+        '}';
+  }
 }
