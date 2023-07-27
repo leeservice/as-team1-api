ccfa6036df1c8ccc0a2fcc791a5c70a4c3867607
ryanbeatt <137309439+ryanbeatt@users.noreply.github.com>
2023-07-27 14:38:18 +0100

    Changes made to validator to fix issue with frontend.

--- src/main/java/org/kainos/ea/validator/JobRoleValidator.java
+++ src/main/java/org/kainos/ea/validator/JobRoleValidator.java
@@ -3,14 +3,19 @@ import org.kainos.ea.model.JobRoleRequest;
 
 public class JobRoleValidator {
     public boolean isValidJobRole(JobRoleRequest jobRoleRequest) {
-        if (jobRoleRequest.getName().length() > 50) {
-            return false;
-        }
-        if (jobRoleRequest.getName().trim() == "") {
-            return false;
-        }
-        if (jobRoleRequest.getUrl().length() > 500) {
-            return false;
+        try {
+            if (jobRoleRequest.getName().length() > 50) {
+                return false;
+            }
+            if (jobRoleRequest.getName().trim() == "") {
+                return false;
+            }
+            if (jobRoleRequest.getUrl().length() > 500) {
+                return false;
+            }
+        } catch (NullPointerException e) {
+            System.err.println(e.getMessage());
+            System.out.println("Null pointer exception called.");
         }
         return true;
     }
