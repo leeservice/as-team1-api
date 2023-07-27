eb9a8252a3c9dece1ca1fc042273880c0dfe390c
ryanbeatt <137309439+ryanbeatt@users.noreply.github.com>
2023-07-27 14:52:27 +0100

    Removed unnecessary try-catch block.

--- src/main/java/org/kainos/ea/validator/JobRoleValidator.java
+++ src/main/java/org/kainos/ea/validator/JobRoleValidator.java
@@ -3,7 +3,6 @@ import org.kainos.ea.model.JobRoleRequest;
 
 public class JobRoleValidator {
     public boolean isValidJobRole(JobRoleRequest jobRoleRequest) {
-        try {
             if (jobRoleRequest.getName().length() > 50) {
                 return false;
             }
@@ -13,10 +12,6 @@ public class JobRoleValidator {
             if (jobRoleRequest.getUrl().length() > 500) {
                 return false;
             }
-        } catch (NullPointerException e) {
-            System.err.println(e.getMessage());
-            System.out.println("Null pointer exception called.");
-        }
         return true;
     }
 }
