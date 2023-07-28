5b14f19e6b31cbfab53cff1cb2161eeb6bc7009a
ryanbeatt <137309439+ryanbeatt@users.noreply.github.com>
2023-07-28 15:32:30 +0100

    Completed integration tests for Auth controller

---
+++ src/test/java/org/kainos/ea/controllerTests/AuthControllerTest.java
@@ -0,0 +1,42 @@
+package org.kainos.ea.controllerTests;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.controller.AuthController;
+import org.kainos.ea.exceptions.DatabaseConnectionException;
+import org.kainos.ea.exceptions.FailedToGenerateTokenException;
+import org.kainos.ea.exceptions.FailedToLoginException;
+import org.kainos.ea.model.LoginRequest;
+import org.kainos.ea.service.AuthService;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTest {
+    AuthService authService = mock(AuthService.class);
+    AuthController authController = new AuthController(authService);
+
+    LoginRequest loginRequest = new LoginRequest("user@kainos.com", "password");
+
+    @Test
+    void login_shouldReturn500_whenAuthServiceThrowsFailedToGenerateTokenException() throws DatabaseConnectionException, SQLException, FailedToLoginException, FailedToGenerateTokenException {
+        when(authService.login(loginRequest)).thenThrow(FailedToGenerateTokenException.class);
+        assertEquals(500, authController.login(loginRequest).getStatus());
+    }
+
+    @Test
+    void login_shouldReturn500_whenAuthServiceThrowsSQLException() throws DatabaseConnectionException, SQLException, FailedToLoginException, FailedToGenerateTokenException {
+        when(authService.login(loginRequest)).thenThrow(SQLException.class);
+        assertEquals(500, authController.login(loginRequest).getStatus());
+    }
+
+    @Test
+    void login_shouldReturn500_whenAuthServiceThrowsDatabaseConnectionException() throws DatabaseConnectionException, SQLException, FailedToLoginException, FailedToGenerateTokenException {
+        when(authService.login(loginRequest)).thenThrow(DatabaseConnectionException.class);
+        assertEquals(500, authController.login(loginRequest).getStatus());
+    }
+}
--- src/test/java/org/kainos/ea/integrationTests/AuthControllerTest.java
+++ src/test/java/org/kainos/ea/integrationTests/AuthControllerTest.java
@@ -5,16 +5,38 @@ import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.model.LoginRequest;
 import org.kainos.ea.trueApplication;
 import org.kainos.ea.trueConfiguration;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class AuthControllerTest {
     static final DropwizardAppExtension<trueConfiguration> APP =
             new DropwizardAppExtension<>(
                     trueApplication.class, null, new ResourceConfigurationSourceProvider());
+
+    @Test
+    void login_shouldReturnToken() {
+        LoginRequest loginRequest = new LoginRequest("user@kainos.com", "user");
+        Response response =
+                APP.client().target("http://localhost:8080/api/login").request().post(Entity.entity(loginRequest, MediaType.APPLICATION_JSON_TYPE));
+        assertEquals(200, response.getStatus());
+        String token = response.readEntity(String.class);
+        assertNotNull(token);
+    }
+
+    @Test
+    void login_shouldReturn400_whenCredentialsInvalid() {
+        LoginRequest loginRequest = new LoginRequest("invaliduser@kainos.com", "invaliduser");
+        Response response =
+                APP.client().target("http://localhost:8080/api/login").request().post(Entity.entity(loginRequest, MediaType.APPLICATION_JSON_TYPE));
+        assertEquals(400, response.getStatus());
+    }
 }
